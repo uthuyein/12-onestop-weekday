9094d87d620dabbbb44eae9bb3e8722e89403211
uthuyein <uthuyeinlinn@gmail.com>
2025-03-20 12:44:05 +0630

    servlet

--- 10-lambda-stream/ep01-lambda/src/main/java/com/jdc/mkt/stream/StreamIntermediateDemo.java
+++ 10-lambda-stream/ep01-lambda/src/main/java/com/jdc/mkt/stream/StreamIntermediateDemo.java
@@ -1,7 +1,6 @@
 package com.jdc.mkt.stream;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class StreamIntermediateDemo {
--- 10-lambda-stream/ep01-lambda/src/main/java/com/jdc/mkt/stream/StreamTerminalDemo.java
+++ 10-lambda-stream/ep01-lambda/src/main/java/com/jdc/mkt/stream/StreamTerminalDemo.java
@@ -3,45 +3,56 @@ package com.jdc.mkt.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamTerminalDemo {
 
-	static List<String> list = new ArrayList<String>(List.of("3","6","1","4","2","1","8","8","7","9","5"));
-	
+	static List<String> list = new ArrayList<String>(List.of("3", "6", "1", "4", "2", "1", "8", "8", "7", "9", "5"));
+
 	public static void main(String[] args) {
-		//useForEachOrdered();
-		//useChecking();
-		singleResult();
+		// useForEachOrdered();
+		// useChecking();
+		// singleResult();
+		// countVsSumAndOther();
+		// reduce();
+		collect();
 	}
-	
-	static void collector() {
-//		list.stream()
-//		.mapToInt(i -> Integer.parseInt(i))
-//		.filter(a -> (a%2) == 0)
-		
+
+	static void collect() {
+		var li = Stream.of(list, List.of("A", "B"))
+				.flatMap(e -> e.stream()).collect(Collectors.toList());
+		System.out.println(li);
 	}
-	
-	static void singleResult() {
-		 list.stream()
-		 .sorted()
-				.findFirst()
-				.ifPresentOrElse(
-						a -> System.out.println(a), 
-						() -> System.out.println("no value"));;
-	}
-	
-	static void useChecking() {
-		var res = list.stream()
-		.mapToInt(n -> Integer.parseInt(n))
-		.filter(a -> (a%2) != 0)
-		.noneMatch(a -> (a % 2 )== 0 );
+
+	static void reduce() {
+		var res = list.stream().distinct().reduce((a, b) -> a + "," + b).orElse("no vaule");
 		System.out.println(res);
-		
 	}
-	
+
+	static void countVsSumAndOther() {
+		var res = list.stream().mapToInt(e -> Integer.parseInt(e)).sorted().distinct()
+				// .count();
+				// .sum();
+				// .min().getAsInt();
+				.max().getAsInt();
+		System.out.println(res);
+
+	}
+
+	static void singleResult() {
+		list.stream().sorted().findFirst().ifPresentOrElse(a -> System.out.println(a),
+				() -> System.out.println("no value"));
+		;
+	}
+
+	static void useChecking() {
+		var res = list.stream().mapToInt(n -> Integer.parseInt(n)).filter(a -> (a % 2) != 0)
+				.allMatch(a -> (a % 2) == 0);
+		System.out.println(res);
+
+	}
+
 	static void useForEachOrdered() {
-		list.parallelStream()
-		.sorted()
-		.forEachOrdered(System.out::println);
+		list.parallelStream().sorted().forEachOrdered(System.out::println);
 	}
 }
